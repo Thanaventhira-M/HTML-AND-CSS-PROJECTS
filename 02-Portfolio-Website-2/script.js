ac3580186a29cbb1310a46e4bdb24ecf77c45366
Thanaventhira-M <thanaventhiran17@gmail.com>
2024-12-21 18:23:53 +0530

    HTML-CSS-PROJECTS

---
+++ 02-Portfolio-Website-2/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".mutiple-text", {
+    strings: ["Software Developer", "Website Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
